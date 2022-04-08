58517aef15477ecddeb61f79fad39d2f4c5e16b0
basniy <yeneyehun@gmail.com>
2022-04-08 14:37:29 -0700

    task c0c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
-*
-*main - Entry point
-*
-*
-*
-*Return: Always 0 (Success)
-*
-*/
+ *
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-puts("\"with proper gramar, but the outcome is a piece of art,\n");
+puts("with proper gramar, but the outcome is a piece of art,\n");
 return (0);
 }
