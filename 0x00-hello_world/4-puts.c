ae3793c146046ab349a5e735c5b1b2783bfccbc5
basniy <yeneyehun@gmail.com>
2022-04-08 09:39:56 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
-#include <stdio.h>
+i#include <stdio.h>
 
 /**
- *
- * *main - Entry point
- *
- * *
- *
- * *Return: Always 0 (Success)
- *
- * */
-
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*
+*/
 int main(void)
-
-	}
-
-	 puts("\"Programming is like building multilingual puzzel");
-
-	 return (0);
-
-        }
+{
+puts("\"Programming is like building multilingual puzzel");
+return (0);
+}
