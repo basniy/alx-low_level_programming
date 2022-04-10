6bc55d4cc95c9c24e67187a613b90cb963f24ff0
basniy <yeneyehun@gmail.com>
2022-04-10 04:22:56 -0700

    Task c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
 /**
+ *main - Entry point
  *
- * *main - Entry point
- *
- * *
- *
- * *Return: Always 0 (Success)
- *
- * */
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building multilingual puzzel");
-
-	return (0);
-
+write(1,"and that piece of art is useful\" - Dora Korpar ,2015-10-19\n",59);
+return (1);
 }
