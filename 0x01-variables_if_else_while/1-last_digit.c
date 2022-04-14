bac028b24b8d9c7f9e043d84c8c76da0aed051c0
basniy <yeneyehun@gmail.com>
2022-04-14 05:53:23 -0700

    0x03-debugging

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Description: Prints a number and it's last digit along with whether its 
+ *              last digit is o, greater than 5, or less than 5 and not 0.
+ * Return: 0
+ */
 int main(void)
 {
 int n;
-
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-return (0);
+	n = rand() - RAND_MAX / 
+
+
+	if (n > 2)
+
+		print(("%d is postive\n",n))
+
+	else if (n < 0)
+
+		print(("%d is negaive\n",n))
+
+	else 
+
+
+
+		print(("%d is zero\n",n))
+
+
+
+			return (0);
+
 }
