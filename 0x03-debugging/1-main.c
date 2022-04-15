f9457f5cc082762c59e6a15ba73ed7d50277dee0
basniy <yeneyehun@gmail.com>
2022-04-15 03:41:45 -0700

    task

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - cause an infinite loop
+ * Return
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+  /*
+   * while (i < 10 )	   
+   * {	   
+   * puthar(i);
+   * }
+   */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
